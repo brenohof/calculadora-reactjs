4d3078a50d2b5a65621af4746914802580ad4dea
Breno H. Oliveira <brenohenriqueof@gmail.com>
2020-02-29 23:00:32 -0300

    modified: button component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -4,5 +4,16 @@ import './Button.css'
 /*
  * Button component is a functional component what means it is stateless. 
  */
-export default props =>
-    <button className='button'>{props.label}</button>
+export default props => {
+    const classes = 'button ' +
+        `${props.operation ? 'operation' : ''}` +
+        `${props.double ? 'double' : ''}` +
+        `${props.triple ? 'triple' : ''}`
+
+    return (
+        <button
+            onClick={e => props.click && props.click(props.label)}
+            className={classes}>
+            {props.label}
+        </button>)
+} 
