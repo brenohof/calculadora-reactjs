5cf2340c06d3c7f1d86f344304db5911196e27bf
Breno H. Oliveira <brenohenriqueof@gmail.com>
2020-03-01 14:01:54 -0300

    Initial content

---
+++ precache-manifest.89c4f151ff06adfe22e599b9f0ad1a9e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30ad45bb9c962842695818cd00559cb1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "23df9536edf6f8799dc6",
+    "url": "/static/css/main.8620af5d.chunk.css"
+  },
+  {
+    "revision": "18266d2c6781ffddcde6",
+    "url": "/static/js/2.5257672c.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.5257672c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23df9536edf6f8799dc6",
+    "url": "/static/js/main.d9b9ede9.chunk.js"
+  },
+  {
+    "revision": "68d3a55def857a34d237",
+    "url": "/static/js/runtime-main.7221cfda.js"
+  },
+  {
+    "revision": "288302ea531af8be59f6ac2b5bbbfdd3",
+    "url": "/static/media/RobotoMono-Thin.288302ea.ttf"
+  }
+]);
